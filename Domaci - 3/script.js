fed4d9760ee5e26fa4b630e7a1851ff7fcce8d9d
Reidi <uros.bo97@gmail.com>
2026-03-07 01:07:48 +0100

    Domaci 3

---
+++ Domaci - 3/script.js
@@ -0,0 +1,142 @@
+
+// Zadatak 1
+let broj = 10;
+if (broj > 5) {
+    console.log("Broj je veci od 5");
+}
+
+// Zadatak 2
+let godine = 18;
+if (godine > 18){
+    console.log("Punoletan");
+}
+
+// Zadatak 3
+let x=3, y=5;
+if (x<y){
+    console.log("x je manji od y");
+}
+
+// Zadatak 4
+let poeni = 70;
+if (poeni >= 60){
+    console.log("Polozio");
+}
+
+// Zadatak 5
+let temperatura = 30;
+if (temperatura > 25){
+    console.log("Vruce je");
+}
+
+// Zadatak 6 Boolean IF
+let jeonline = true;
+if (jeonline == true){
+    console.log("Korisnik je online");
+}
+
+// Zadatak 7
+let imaNalog = false;
+if (imaNalog == false){
+    console.log("Nema Nalog");
+}
+
+//Zadatak 8
+let ulogovan = true;
+if (ulogovan == true){
+    console.log("Uspesno ste ulogovani");
+}
+
+//Zadatak 9
+let punoletan = true;
+if (punoletan == true){
+    console.log("Dozvoljen Pristup");
+}
+
+//Zadatak 10
+let jeAdmin = false;
+if (jeAdmin == false){
+    console.log("Nemate Admin Prava");
+}
+// String i IF
+//Zadatak 11
+let ime= "Ana";
+if (ime == "Ana"){
+    console.log("Zdravo Ana");
+}
+
+//Zadatak 12
+let grad = "Beograd";
+if (grad == "Beograd"){
+    console.log("Glavni Grad");
+}
+//Zadatak 13
+let jezik = "JavaScript"
+if (jezik == "JavaScript"){
+    console.log("Ucis JS");
+}
+//Zadatak 14
+let status = "online";
+if (status == "online"){
+    console.log("Korisnik je Online");
+}
+//Zadatak 15
+let dan = "Ponedeljak";
+if (dan == "Ponedeljak"){
+    console.log("Pocetak Nedelje");
+}
+//Array i IF
+//Zadatak 16
+let brojevi = [1, 2, 3];
+if (brojevi[0] > 0){
+    console.log("Pozitivan Broj");
+}
+//Zadatak 17
+let imena = ["Ana", "Marko"];
+if (imena[0] == "Ana"){
+    console.log("Pronadjena Ana");
+}
+//Zadatak 18
+let ocene = [5, 4, 3];
+if(ocene[0] == 5){
+    console.log("Oclican");
+}
+//zadatak 19
+let statusi = [true, false];
+if (statusi[1] == false){
+    console.log("Offline");
+}
+//Zadatak 20
+let jezici = ["HTML", "CSS", "JavaScript"];
+if (jezici[2] == "JavaScript"){
+    console.log("JS pronadjen");
+}
+//Kombinovane vezbe
+//Zadatak 21
+let broj2 = 8;
+if (broj2%2 == 0){
+    console.log("Paran Broj");
+}else
+{
+    console.log("Nije paran");
+}
+//Zadatak 22
+let rezultat=45;
+if (rezultat < 50){
+    console.log("Nedovoljan");
+}
+//Zadatak 23
+let lozinka = "1234";
+if (lozinka == "1234"){
+    console.log("Slaba Lozinka");
+}
+//Zadatak 24
+let poruka = "Ucim JavaScript";
+if (poruka.includes("JavaScript")) {
+    console.log("JS poruka");
+}
+//Zadatak 25
+let korisnici = ["Admin", "user"];
+if (korisnici[0] == "Admin"){
+    console.log("Admin Pristup");
+}
