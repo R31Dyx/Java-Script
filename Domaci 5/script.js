25b8e5301d7b8781aaaae91154ae3021dac46342
Reidi <uros.bo97@gmail.com>
2026-03-10 12:10:48 +0100

    Domaci 5

--- Domaci 5/script.js
+++ Domaci 5/script.js
@@ -154,9 +154,159 @@ for (let i = 0; i < brojevi2.length; i++) {
 console.log("Najveci broj u listi je: "+veci);
 
 //Zadatak 14
-let poruke = ["Hello", "World", "JS", "Toza"];
+let poruke = ["Hello", "World", "JS", "Toza","Ognjen"];
 for (let i = 0; i < poruke.length; i++) {
-    if (poruke[i].includes("o")){
+    if (poruke[i].toLowerCase().includes("o")){
         console.log("Rec "+poruke[i]+" sadrzi slovo 'O'");
+    }
+}
+//Zadatak 15
+let poeni = 85;
+if (poeni >= 80){
+    console.log("Odlican");
+}else if (poeni >= 60 && poeni <= 79){
+    console.log("Dobar");
+}else if (poeni < 60){
+    console.log("Nedovoljan");
+}
+//Zadatak 16
+let uloge = ["admin", "user","guest"];
+for (let i = 0; i < uloge.length; i++) {
+    switch (uloge[i].toLowerCase()) {
+        case "admin":
+            console.log("Admin Pristup");
+            break;
+            case "user":
+                console.log("Korisnik Pristup");
+                break;
+                case "guest":
+                    console.log("Gost Pristup");
+                    break;
+        default:
+            console.log("Nepoznat korisnik");
+            break;
+    }
+}
+//Zadatak 17
+let brojeviNiz = [2,4,6,8];
+let i=0;
+while (i<brojeviNiz.length) {
+    console.log(brojeviNiz[i]);
+    i++;
+}
+ //Zadatak 18
+let x=5;
+while (x>=0) {
+    //while (x>0) da izbegnemo da x ne dobije vrednost 0 (min ce biti 1)
+    console.log("X je sada: "+x);
+    x--;
+}
+//zadatak 19
+let ocene1 = [5,4,3,2,1,4];
+let sveOcene =0;
+for (let i = 0; i < ocene1.length; i++) {
+    sveOcene = sveOcene + ocene1[i];
+}
+console.log("Prosek svih ocena je: "+sveOcene/ocene1.length);
+
+//Zadatak 20
+let jezici = ["JS","HTML","CSS"];
+for (let i = 0; i < jezici.length; i++) {
+    switch (jezici[i].toLowerCase()) {
+        case "js":
+            console.log("Ovo je JavaScript");break;
+            case "html":
+                console.log("Ovo je HTML");break;
+                case "css":
+                    console.log("Ovo je CSS");break;
+        default:
+            console.log("Nepoznat jezik");break;
+    }
+}
+//Zadatak 21
+let ulogovan = [true, false, true];
+for (let i = 0; i < ulogovan.length; i++) {
+    if (ulogovan[i]){
+        console.log("Korisnik je Ulogovan");
+    }else{
+        console.log("Korisnik nije Ulogovan");
+    }
+}
+//Zadatak 22
+let vreme = "Noc";
+switch (vreme.toLowerCase()) {
+    case "noc":
+        console.log("Laku noc");break;
+    case "dan":
+        console.log("Dobar Dan");break;
+    case "jutro":
+        console.log("Dobro Jutro");break;
+        default:
+            console.log("Sumrak Saga");break;
+}
+//Zadatak 23
+let cifre2 = [1,2,3,4,5,5,7];
+let sabir = 0;
+for (let i = 0; i < cifre2.length; i++) {
+    if(cifre2[i]>3){
+        sabir = sabir + cifre2[i];
+    }
+} console.log("Zbir svih brojeva vecih od 3 je: "+sabir);
+
+//Zadatak 24
+let opetOcene = [5,3,4,2,5];
+let brojac = 0;
+for (let i = 0; i < opetOcene.length; i++) {
+    if (opetOcene[i] === 5) {
+        brojac++;
+    }
+} console.log("Petica imamo: "+brojac);
+
+//Zadatal 25
+let opetGradovi = ["nes","Beograd", "Novi Sad", "Nis","NoviGrad","Subotica"];
+for (let i = 0; i < opetGradovi.length; i++) {
+    if (opetGradovi[i].toLowerCase() === "nis") {
+        console.log("Nasli smo Nis");break;
+        //break je tu da prekine for ako nadje nis. nema potrebe da vrti ako vec imamo nis
+    }
+    console.log("Nismo nasli Nis");
+}
+//Zadatak 26
+let brojevi3=[1,2,3,4,5,6,7];
+for (let i = 0; i < brojevi3.length; i++) {
+    brojevi3[i] += 10;
+}
+console.log("Nas niz sada izgleda ovako "+brojevi3);
+//Zadatak 27
+let tip = "Mobilni";
+switch (tip.toLowerCase()) {
+    case "mobilni":
+        console.log("Mobilni uredjaj."); break;
+    case"desktop":
+        console.log("Desktop Uredjaj"); break;
+    default:
+            console.log("Nepoznat uredjaj");break;
+}
+//Zadatak 28
+let poruke1 = ["Zdravo","Cao","Hello"];
+let spojena = "";
+for (let i = 0; i < poruke1.length; i++) {
+    spojena += poruke1[i];
+}
+console.log(spojena);
+//Zadatak 29
+let brojevi4=[10,20,30,40];
+for (let i = 0; i < brojevi4.length; i++) {
+    console.log("Podeljen sa 2 iznosti: "+brojevi4[i]/2);
+}
+//Zadatak 30
+let ocene3 = [5,3,4,2,5];
+for (let i = 0; i < ocene3.length; i++) {
+    if (ocene3[i] >= 5) {
+        console.log("Odlican");
+    }else if (ocene3[i] < 5 && ocene3[i] > 2) {
+        console.log("Dobar");
+    }else{
+        console.log("Nedovoljan");
     }
 }
